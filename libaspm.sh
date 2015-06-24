3c448c6d5057d72876bdac21d0f82e8619c2b0cf
Sekadar Awan <sekadar.awan@gmail.com>
2015-06-25 04:46:54 +0700

    Edit refactor

--- libaspm.sh
+++ libaspm.sh
@@ -5,11 +5,44 @@ check_package() {
   # stub belum tau mau di isi apaan
 }
 
+# get_aspkg() pkg_name
+# return value:
+# 0 - Success
+# 1 - Not found
+# 2 - Multiple
+get_aspkg() {
+  local pkg_name tmp count
+
+  tmp=($( split_pkg_ver "${1}" ))
+  count="$( pkg_repo ${tmp[0]} ${PKGLIST} | wc -l )"
+  pkg_name="$( pkg_repo ${tmp[0]} ${PKGLIST} )"
+  case ${count} in
+    0)
+      return 1
+      ;;
+    1)
+      ;;
+    *)
+      echo "${pkg_name}"
+      return 2
+      ;;
+  esac
+
+  if [ -n "${tmp[1]}" ]; then
+    pkg_name="${ASPKG_DIR}/${pkg_name}/aspkg-${tmp[1]}"
+  else
+    pkg_name="${ASPKG_DIR}/${pkg_name}/aspkg"
+  fi
+
+  echo "${pkg_name}"
+  return 0
+}
+
 split_pkg_ver() {
   echo "${1}" | sed -r 's/[-:]/ /'
 }
 
 pkg_repo() {
-  grep -E '[/]*[^[:alpha:]]*'${1}'$' "${2}"
+  grep -E '[/]*[^[:alpha:]]'${1}'$' "${2}"
 }
 # vim: ft=sh:ts=4:sw=2:sts=2:syn=sh:et
